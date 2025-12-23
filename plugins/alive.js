ba8e1782502854cb8390e8977a5dbc2f11b330b3
udaralakmal2025-web <udaralakmal2025@gmail.com>
2025-12-23 13:30:58 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -7,14 +7,14 @@ cmd({
     category: "main",
     filename: __filename
 },
-async (danuwa, mek, m, {
+async (UDARA_MD, mek, m, {
     from, quoted, body, isCmd, command, args, q, isGroup,
     sender, senderNumber, botNumber2, botNumber, pushname,
     isMe, isOwner, groupMetadata, groupName, participants,
     groupAdmins, isBotAdmins, isAdmins, reply
 }) => {
     try {
-        return await danuwa.sendMessage(from, {
+        return await UDARA_MD.sendMessage(from, {
             image: { url: config.ALIVE_IMG },
             caption: config.ALIVE_MSG
         }, { quoted: mek });
